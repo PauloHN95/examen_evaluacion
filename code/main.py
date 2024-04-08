14d078a73f9e6563b6307ddc1a5a39b5fef96851
Paulo Henriques Neira <paulohenriquesneira@gmail.com>
2024-04-09 00:49:15 +0200

    Ultimos docstrings de main() con el input alumno

--- code/main.py
+++ code/main.py
@@ -2,6 +2,12 @@ from data_reader import leer_datos,filtrar_calcular_media
 import pandas as pd
 
 def main():
+  """
+  df: es el df que se crea con la informacion del csv
+  alumno: es un input y nos pedira por consola el nombre del alumno que queremos sacar su tag
+    con el formato Examen_Nombre.
+  media: media de la columna value del alumno seleccionado  
+  """
   df = leer_datos('code\..\data\datos_examen.csv')
   alumno = input('Dime el tag del alumno que quieres buscar  formato -> Examen_Nombre: ')
   media = filtrar_calcular_media(df,alumno)
